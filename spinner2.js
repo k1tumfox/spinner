a2296a35b3c3de0770de3bd40400a27183c8d679
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-25 16:34:18 +0000

    refactored spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,41 +1,10 @@
-let animate = ['| ', '\r/ ', '\r-- ', '\r\\ ', '\r| '];
-let delay = 50;
-animate.forEach(ele => {
+const blinker = (anim, delay) => {
   setTimeout(() => {
-    process.stdin.write(ele);
+    process.stdout.write(`\r${anim}   `); 
   }, delay);
-  delay += 100;
-});
-
-setTimeout(() => {
-  process.stdout.write('\n');
-}, 550);
-
-//can't feed an array into process.stdout.write?..
-
-// for (ele of animate) {
-//   setTimeout(() => {
-//     process.stdout.write(ele);  
-//   }, delay);
-//   delay += 100;
-// }
-
-
-
-//PREVIOUS
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
+};
+blinker('|', 100);
+blinker('/', 300);
+blinker('-', 500);
+blinker('\\', 700);
+blinker('|', 900);
